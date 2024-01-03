327efc356014a9d52199807b454c8dd4042a27b5
matternstsr <matternstsr@gmail.com>
2024-01-03 17:50:28 -0600

    ?? for parameters are null or undefined

---
+++ ES6_basic/3-default-parameter.js
@@ -0,0 +1,7 @@
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+  // used the nullish coalescing operator (??) along with default parameter values.
+  // ?? for parameters are null or undefined
+  expansion1989 = expansion1989 ?? 89;
+  expansion2019 = expansion2019 ?? 19;
+  return initialNumber + expansion1989 + expansion2019;
+}
