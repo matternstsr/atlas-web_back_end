6367bf8a8441820f523cba6fd1cc80d904d3832d
matternstsr <matternstsr@gmail.com>
2024-01-03 17:23:44 -0600

    no unused?

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,8 +1,10 @@
 export default function taskBlock(trueOrFalse) {
+  // eslint-disable-next-line no-unused-vars
   const task = false; // cant use let
   const task2 = true; // cant use let
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
     const task = true; // cant use let
     const task2 = false; // cant use let
   }
