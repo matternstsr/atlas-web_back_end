e9986369c38c88c37a93ff034c5a1e73848a8ddc
matternstsr <matternstsr@gmail.com>
2024-01-03 22:56:01 -0600

    not sure why i cant comment

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,10 +1,3 @@
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
-// export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-// used the nullish coalescing operator (??) along with default parameter values.
-// ?? for parameters are null or undefined
-// expansion1989 = expansion1989 ?? 89;
-// expansion2019 = expansion2019 ?? 19;
-// return initialNumber + expansion1989 + expansion2019;
-// }
