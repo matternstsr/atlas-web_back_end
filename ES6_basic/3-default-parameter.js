3a533283d998bfec389ef01dd362d71c353b377d
matternstsr <matternstsr@gmail.com>
2024-01-03 22:50:13 -0600

    cant use ?? for parameters are null or undefined

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,10 +1,10 @@
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-  // export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  // used the nullish coalescing operator (??) along with default parameter values.
-  // ?? for parameters are null or undefined
-  // expansion1989 = expansion1989 ?? 89;
-  // expansion2019 = expansion2019 ?? 19;
-  // return initialNumber + expansion1989 + expansion2019;
-  // }
   return initialNumber + expansion1989 + expansion2019;
 }
+// export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+// used the nullish coalescing operator (??) along with default parameter values.
+// ?? for parameters are null or undefined
+// expansion1989 = expansion1989 ?? 89;
+// expansion2019 = expansion2019 ?? 19;
+// return initialNumber + expansion1989 + expansion2019;
+// }
