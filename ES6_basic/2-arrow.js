360bfa82fef51d0230825b0a655d35056a5d78c2
matternstsr <matternstsr@gmail.com>
2024-01-03 23:04:06 -0600

    took the comments out for 2 arrow as well

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,8 +1,5 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-  // Using arrow function nstead of 'this' and used the this from func above
-	// uses ES6 arrow syntax for the "addNeighborhood" method.
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
