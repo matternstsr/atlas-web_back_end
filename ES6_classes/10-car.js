b81d3ec62553dfd3ee10f45341489725c510a1fc
matternstsr <matternstsr@gmail.com>
2024-01-05 17:48:02 -0600

    hmm what is the deal why wont this work?

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,16 @@
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
+class Car {
+  constructor(brand, motor, color) {
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
+  }
+
+  cloneCar() {
+    const stolenCar = new this.constructor(this[_brand], this[_motor], this[_color]);
+    return stolenCar;
+  }
+}
