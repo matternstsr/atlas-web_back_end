3b0f4975692fb8ab22c9a8feb88ef8b19780377d
matternstsr <matternstsr@gmail.com>
2024-01-05 14:22:37 -0600

    going back that didnt work

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this.validateAndSetString(name, 'Name');
-    this._length = this.validateAndSetNumber(length, 'Length');
-    this._students = this.validateAndSetStudentsArray(students);
+    this.validateAndSetString(name, 'Name');
+    this.validateAndSetNumber(length, 'Length');
+    this.validateAndSetStudentsArray(students);
   }
 
   get name() {
@@ -10,7 +10,7 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
-    this._name = this.validateAndSetString(newName, 'Name');
+    this.validateAndSetString(newName, 'Name');
   }
 
   get length() {
@@ -18,7 +18,7 @@ export default class HolbertonCourse {
   }
 
   set length(newLength) {
-    this._length = this.validateAndSetNumber(newLength, 'Length');
+    this.validateAndSetNumber(newLength, 'Length');
   }
 
   get students() {
@@ -26,27 +26,27 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
-    this._students = this.validateAndSetStudentsArray(newStudents);
+    this.validateAndSetStudentsArray(newStudents);
   }
 
   validateAndSetString(value, attribute) {
     if (typeof value !== 'string') {
       throw new TypeError(`${attribute} must be a string`);
     }
-    return value;
+    this[`_${attribute.toLowerCase()}`] = value;
   }
 
   validateAndSetNumber(value, attribute) {
     if (typeof value !== 'number' || Number.isNaN(value)) {
       throw new TypeError(`${attribute} must be a number`);
     }
-    return value;
+    this[`_${attribute.toLowerCase()}`] = value;
   }
 
   validateAndSetStudentsArray(students) {
     if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-    return students;
+    this._students = students;
   }
 }
