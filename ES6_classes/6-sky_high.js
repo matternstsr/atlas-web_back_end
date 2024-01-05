33094f421f19197f9f435592697ea4b69b7836ea
matternstsr <matternstsr@gmail.com>
2024-01-05 16:28:42 -0600

    require wasa no go

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,17 +1,19 @@
-class SkyHighBuilding extends require('./5-building.js').default {
-    constructor(sqft, floors) {
-      super(sqft);
-      this._floors = floors;
-    }
-  
-    get floors() {
-      return this._floors;
-    }
-  
-    evacuationWarningMessage() {
-      return `Evacuate slowly the ${this._floors} floors.`;
-    }
+// 6-sky_high.js
+import Building from './5-building.js';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
   }
-  
-  export default SkyHighBuilding;
-  
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
+
+export default SkyHighBuilding;
