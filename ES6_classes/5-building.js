3ec4d0c1e07e3ba96daf0df158b9b2ea89ecdb92
matternstsr <matternstsr@gmail.com>
2024-01-05 15:54:55 -0600

    that didnt work going back

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 export default class Building {
   constructor(sqft) {
-    if (!(this instanceof Building) && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+    if (!(this instanceof Building) || typeof this.evacuationWarningMessage !== 'function') {
+			throw new Error('Class extending Building must override evacuationWarningMessage');
+		}
 
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
