ac046b3dfecf62c92982f0bf80a6eb81cb139780
matternstsr <matternstsr@gmail.com>
2024-01-05 12:44:20 -0600

    sending what i have and see what ive done wrong

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,52 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = this.validateString(name, 'Name');
+    this._length = this.validateNumber(length, 'Length');
+    this._students = this.validateStudentsArray(students);
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = this.validateString(newName, 'Name');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    this._length = this.validateNumber(newLength, 'Length');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    this._students = this.validateStudentsArray(newStudents);
+  }
+
+  validateString(value, attribute) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attribute} must be a string`);
+    }
+    return value;
+  }
+
+  validateNumber(value, attribute) {
+    if (typeof value !== 'number' || isNaN(value)) {
+      throw new TypeError(`${attribute} must be a number`);
+    }
+    return value;
+  }
+
+  validateStudentsArray(students) {
+    if (!Array.isArray(students) || students.some(student => typeof student !== 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    return students;
+  }
+}
