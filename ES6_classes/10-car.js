9d138d821fa0a7ac39fbc9abc05d76f6e46bcb6d
matternstsr <matternstsr@gmail.com>
2024-01-05 18:30:39 -0600

    trying this before i use spicies like travis is talking about

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,17 +1,17 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
 
 class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    const cloneCar = new this.constructor(this[_brand], this[_motor], this[_color]);
-    return cloneCar;
+    const clonedCar = new this.constructor();
+    Object.keys(this).forEach((key) => {
+      clonedCar[key] = this[key];
+    });
+    return clonedCar;
   }
 }
 
