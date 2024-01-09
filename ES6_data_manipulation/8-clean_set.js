98e8db7f7e013a1c7d5c9b5e127ffc0613063fe6
matternstsr <matternstsr@gmail.com>
2024-01-09 17:28:50 -0600

    return as string?  return ''

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,12 @@
 function cleanSet(set, startString = '') {
+  if (typeof startString !== 'string') {
+    return '';
+  }
+
   return Array.from(set)
-    .filter((value) => value.startsWith(startString))
+  .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
 }
+
 export default cleanSet;
