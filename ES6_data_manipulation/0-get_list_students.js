955ad4e9f4d037b3c8014d992b38563aefb35332
matternstsr <matternstsr@gmail.com>
2024-01-09 12:23:09 -0600

    6:62  error  Missing trailing comma                                           comma-dangle

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,14 @@
-
 function getListStudents() {
   const studentsData = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 
   return studentsData.map(({ id, firstName, location }) => ({
     id,
     firstName,
-    location
+    location,
   }));
 }
 
