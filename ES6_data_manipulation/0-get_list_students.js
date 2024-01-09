1eb8674b1fd103880ec9ad79983507a88751b736
matternstsr <matternstsr@gmail.com>
2024-01-09 12:18:19 -0600

    1:10  error  'getListStudents' is defined but never used  no-unused-vars

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,16 @@
+
 function getListStudents() {
-  return [
+  const studentsData = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
+
+  return studentsData.map(({ id, firstName, location }) => ({
+    id,
+    firstName,
+    location
+  }));
 }
+
+export default getListStudents;
