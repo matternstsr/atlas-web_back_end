63a432d5596f676e31f5711c8d62d9b2c8d2bb2b
matternstsr <matternstsr@gmail.com>
2024-01-09 14:53:56 -0600

    oops messed that up

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 function updateStudentGradeByCity(students, city, newGrades) {
   return students
-  .filter((student) => student.location === city)
+    .filter((student) => student.location === city)
     .map((student) => {
       const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
       return {
         ...student,
         grade: matchingGrade ? matchingGrade.grade : 'N/A',
-    };
+      };
     });
 }
 export default updateStudentGradeByCity;
