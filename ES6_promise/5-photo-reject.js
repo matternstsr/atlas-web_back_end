5237132d40816cea31b321222f26466e8ce5c226
matternstsr <matternstsr@gmail.com>
2024-01-10 16:17:11 -0600

    no resolve at all and do it for all not just jpg

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,4 @@
+
 export default function uploadPhoto(filename) {
-  if (!filename.endsWith('.jpg')) {
-    return Promise.reject(new Error(`${filename} cannot be processed`));
-  }
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
