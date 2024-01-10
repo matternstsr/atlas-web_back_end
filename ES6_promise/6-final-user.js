0e8ef135337aafd07ddef4d46c231286c9f7ea01
matternstsr <matternstsr@gmail.com>
2024-01-10 16:27:58 -0600

    about brain fried...

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => {
+      return results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      }));
+    });
+}
