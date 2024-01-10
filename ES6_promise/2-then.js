edb52fb7aa7b16f50fbe1b92534177d8b2c864b2
matternstsr <matternstsr@gmail.com>
2024-01-10 15:10:40 -0600

    returning new error on both...

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,13 +2,10 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
+      return new Error();
     })
     .catch((error) => {
       console.log('Got a response from the API with an error:', error);
-      throw Error('Error from handleResponseFromAPI');
+      throw new Error('Error from handleResponseFromAPI');
     });
 }
