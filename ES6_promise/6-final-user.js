5abf85b833711ddac524627963811941a6dfa86c
matternstsr <matternstsr@gmail.com>
2024-01-10 16:50:56 -0600

    added a catch for the erorr...

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName).catch((error) => error), // Catch the rejection and treat it as a resolved promise
-  ]).then((results) => {
-    return results.map((result, index) => ({
-      status: index === 0 ? 'fulfilled' : 'rejected',
-      value: result,
-    }));
-  });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const [usersData, filesData] = await Promise.all([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ]);
+
+    return [
+      { status: 'fulfilled', value: usersData },
+      { status: 'fulfilled', value: filesData },
+    ];
+  } catch (error) {
+    return [
+      { status: 'rejected', value: error.toString() },
+      { status: 'rejected', value: error.toString() },
+    ];
+  }
 }
