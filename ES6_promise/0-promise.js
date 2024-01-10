4bcf4f3b5e9ee61d4b13fc927d465dcc093c5da0
matternstsr <matternstsr@gmail.com>
2024-01-10 14:53:26 -0600

    4:15  error  Strings must use singlequote        quotes

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("API response data");
+      resolve('API response data');
     }, 1000);
   });
 }
