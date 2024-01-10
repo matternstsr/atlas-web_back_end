c8139aac0d365383010f4419ad0f9dd06a9bb513
matternstsr <matternstsr@gmail.com>
2024-01-10 16:02:49 -0600

    added the new for the error back in now that reject is working

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,9 +1,9 @@
 export default function uploadPhoto(fileName) {
-  return Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (fileName.endsWith('.jpg')) {
       resolve(`File ${fileName} uploaded successfully`);
     } else {
-      reject(Error(`${fileName} cannot be processed`));
+      reject(new Error(`${fileName} cannot be processed`));
     }
   });
 }
