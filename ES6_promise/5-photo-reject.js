a3841aee54a5ceb5bfab0d5019b6b5298fad3c5d
matternstsr <matternstsr@gmail.com>
2024-01-10 16:13:48 -0600

    took the test error for passing out

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -2,5 +2,4 @@ export default function uploadPhoto(filename) {
   if (!filename.endsWith('.jpg')) {
     return Promise.reject(new Error(`${filename} cannot be processed`));
   }
-  return Promise.resolve(`File ${filename} uploaded successfully`);
 }
