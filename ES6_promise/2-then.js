a1170a0a2a77d92ed0f66c72e9afdfe7ce221f13
matternstsr <matternstsr@gmail.com>
2024-01-10 15:12:44 -0600

    about out of ideas new didnt work

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,10 +2,13 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-      return new Error();
+      return {
+        status: 200,
+        body: 'success',
+      };
     })
     .catch((error) => {
       console.log('Got a response from the API with an error:', error);
-      throw new Error('Error from handleResponseFromAPI');
+      return error;
     });
 }
