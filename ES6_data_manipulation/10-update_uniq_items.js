4d9cc31185c6142d576438a20949c77d1d24fb14
matternstsr <matternstsr@gmail.com>
2024-01-09 18:06:44 -0600

    10??

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  groceriesMap.forEach((quantity, item) => {
+    if (quantity === 1) {
+      groceriesMap.set(item, 100);
+    }
+  });
+}
