d6b92d9c30b4b255979fc47cbefeac00eafc66bb
matternstsr <matternstsr@gmail.com>
2024-01-10 16:58:06 -0600

    fine!!! ill do the then and c

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,20 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const [usersData, filesData] = await Promise.all([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ]);
+  const usersDataPromise = signUpUser(firstName, lastName)
+    .then((data) => ({ status: 'fulfilled', value: data }))
+    .catch((err) => ({ status: 'rejected', value: err.toString() }));
 
-    return [
-      { status: 'fulfilled', value: usersData },
-      { status: 'fulfilled', value: filesData },
-    ];
-  } catch (errorArray) {
-    return errorArray.map((error) => ({
-      status: 'rejected',
-      value: error.toString(),
-    }));
-  }
+  const filesDataPromise = uploadPhoto(fileName)
+    .then((data) => ({ status: 'fulfilled', value: data }))
+    .catch((err) => ({ status: 'rejected', value: err.toString() }));
+
+  const [usersData, filesData] = await Promise.all([usersDataPromise, filesDataPromise]);
+
+  return [usersData, filesData];
 }
