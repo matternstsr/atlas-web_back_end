e95fa722d4f3cf1bf638c89ba7a851dbebcf79ff
matternstsr <matternstsr@gmail.com>
2024-01-10 20:54:31 -0600

    cant use catch again its finally

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -6,7 +6,7 @@ export default function guardrail(mathFunction) {
     queue.push(result);
   } catch (error) {
     queue.push(`Error: ${error.message}`);
-  } catch {
+  } finally {
     queue.push('Guardrail was processed');
     return queue;
   }
